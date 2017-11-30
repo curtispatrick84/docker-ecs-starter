77cd2a423e2d9f752642e9e9781e6ba13c21e6bf
cpatrick <curtis.patrick@nimblestorage.com>
2017-11-29 20:37:00 -0800

    gitignore update

--- ecs-push.sh
+++ ecs-push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-$(aws ecr get-login --region us-west-1 --profile lucio)
+$(aws ecr get-login --region us-west-2 --profile pp --no-include-email)
 docker build -t docker-starter .
-docker tag docker-starter:latest 613530814445.dkr.ecr.us-west-1.amazonaws.com/docker-starter:latest
-docker push 613530814445.dkr.ecr.us-west-1.amazonaws.com/docker-starter:latest
-aws ecs stop-task --profile lucio --cluster prototypes --region us-west-1 --task $(aws ecs list-tasks --profile lucio --cluster prototypes --family docker-starter --region us-west-1 | grep arn | sed 's/"//g' | awk '{print $1}') | grep 'desiredStatus\|taskDefinitionArn'
+docker tag docker-starter:latest 441599757612.dkr.ecr.us-west-2.amazonaws.com/docker-starter:latest
+docker push 441599757612.dkr.ecr.us-west-2.amazonaws.com/docker-starter:latest
+aws ecs stop-task --profile pp --cluster pretty-pallets --region us-west-2 --task $(aws ecs list-tasks --profile lucio --cluster prototypes --family docker-starter --region us-west-1 | grep arn | sed 's/"//g' | awk '{print $1}') | grep 'desiredStatus\|taskDefinitionArn'
